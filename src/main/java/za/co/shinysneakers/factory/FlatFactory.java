095938ad6a5e4338b62deecbaf927d01ee4da641
Lennox <lennox.lee1927@gmail.com>
2025-05-08 22:52:27 +0200

    flatFactory with metod to validate string Attributes

---
+++ src/main/java/za/co/shinysneakers/factory/FlatFactory.java
@@ -0,0 +1,31 @@
+package za.co.shinysneakers.factory;
+
+import za.co.shinysneakers.domain.Flat;
+import za.co.shinysneakers.domain.House;
+import za.co.shinysneakers.util.Helper;
+
+public class FlatFactory {
+    public static Flat validateStringAttribbutesAndCreateHouse(String flatName,
+                                                               String streetName,
+                                                               String suburb,
+                                                               String city,
+                                                               String province
+    ){
+        if(
+                Helper.isNullOrEmpty(flatName)
+                &&Helper.isNullOrEmpty(streetName)
+                &&Helper.isNullOrEmpty(suburb)
+                &&Helper.isNullOrEmpty(city)
+                &&Helper.isNullOrEmpty(province)) {
+            ;
+            return null;
+        }
+        return new Flat.Builder()
+                .setFlatName(flatName)
+                .setStreetName(streetName)
+                .setSuburb(suburb)
+                .setCity(city)
+                .setProvince(province)
+                .build();
+    }
+}
