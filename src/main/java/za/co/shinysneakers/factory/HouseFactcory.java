e66fbc07625a65241fd76b4fafdf8dce924df2f2
Lennox <lennox.lee1927@gmail.com>
2025-05-08 21:51:14 +0200

    factory package and the House class wit one method validating String Attributes

---
+++ src/main/java/za/co/shinysneakers/factory/HouseFactcory.java
@@ -0,0 +1,26 @@
+package za.co.shinysneakers.factory;
+
+import za.co.shinysneakers.domain.House;
+import za.co.shinysneakers.util.Helper;
+
+public class HouseFactcory {
+    public static House validatesStringAttribbutesAndCreateHouse(String streetName,
+                                                                 String suburb,
+                                                                 String city,
+                                                                 String province
+                                                                 ){
+        if(Helper.isNullOrEmpty(streetName)
+                &&Helper.isNullOrEmpty(suburb)
+                &&Helper.isNullOrEmpty(city)
+                &&Helper.isNullOrEmpty(province)) {
+            ;
+            return null;
+        }
+        return new House.Builder()
+                .setStreetName(streetName)
+                .setSuburb(suburb)
+                .setCity(city)
+                .setProvince(province)
+                .build();
+    }
+}
